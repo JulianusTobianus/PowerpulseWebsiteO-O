110f0a6f040f7e786777e55d5aaa1961e9bb46c3
JulianusTobianus <gwntobiasss@gmail.com>
2025-11-16 20:30:34 +0100

    Add files via upload

--- Javascript.js
+++ Javascript.js
@@ -2,13 +2,29 @@ const numberDisplay = document.getElementById("number");
 const increaseBtn = document.getElementById("increase");
 const decreaseBtn = document.getElementById("decrease");
 
-let number = 0;
+if (numberDisplay) {
 
-increaseBtn.addEventListener("click", () => {
-  number++;
-  numberDisplay.textContent = number;
-});
-decreaseBtn.addEventListener("click", () => {
-  number--;
-  numberDisplay.textContent = number;
-});
+   
+    let number = Number(localStorage.getItem("savedNumber")) || 1;
+
+    numberDisplay.textContent = number;
+
+    if (increaseBtn && decreaseBtn) {
+
+        increaseBtn.addEventListener("click", () => {
+            number++;
+            numberDisplay.textContent = number;
+
+            localStorage.setItem("savedNumber", number);
+        });
+
+        decreaseBtn.addEventListener("click", () => {
+            if (number > 1) {
+                number--;
+            }
+            numberDisplay.textContent = number;
+
+            localStorage.setItem("savedNumber", number);
+        });
+    }
+}
